cef9ebbe763179166a1a10574798ec96cbe506e6
Faisal Aziz <faisal.aziz@ntdac.naztech.local>
2019-01-17 18:16:48 +0600

    Completed 14 conversions

--- src/main/java/com/faisal/dateTimeExtend/DateTime/Convertion.java
+++ src/main/java/com/faisal/dateTimeExtend/DateTime/Convertion.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.sql.Time;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.Calendar;
 
 public class Convertion {
@@ -46,5 +50,50 @@ public class Convertion {
 	public static Time convertLocalTimeToTime(LocalTime localTime) 
 	{
 		return Time.valueOf(localTime);
+	}
+	
+	public static OffsetDateTime  convertLocalDateToOffsetDate(LocalDateTime ldt)
+	{
+		return OffsetDateTime.of(ldt, ZoneOffset.UTC);
+	}
+	
+	public static LocalDateTime convertCalanderToLocalDateTime(Calendar cal) {
+		return LocalDateTime.of(cal.get(Calendar.YEAR),
+		        cal.get(Calendar.MONTH),
+		        cal.get(Calendar.DAY_OF_MONTH),
+		        cal.get(Calendar.HOUR),
+		        cal.get(Calendar.MINUTE),
+		        cal.get(Calendar.SECOND));
+		
+	}
+	
+	public static Instant convertOffsetDateTimeToInstant(OffsetDateTime odt)
+	{
+		return odt.toInstant();
+	}
+	
+	public static LocalDate convertOffsetDateTimeToLocalDate(OffsetDateTime odt)
+	{
+		return odt.toLocalDate();
+	}
+	
+	public static LocalDateTime convertOffsetDateTimeToLocalDateTime(OffsetDateTime odt)
+	{
+		return odt.toLocalDateTime();
+	}
+	
+	public static OffsetDateTime convertLocalDateTimeToOffsetDateTime(LocalDateTime date)
+	{
+		return date.atOffset(ZoneOffset.ofHours(6));	
+	}
+	
+	public static LocalTime convertOffsetDateTimeToLocalTime(OffsetDateTime odt)
+	{
+		return odt.toLocalTime();
+	}
+	
+	public static OffsetTime convertOffsetDateTimeToOffsetTime(OffsetDateTime odt)
+	{
+		return odt.toOffsetTime();
 	}
 }
--- src/test/java/com/faisal/dateTimeExtend/DateTime/ConvertionTest.java
+++ src/test/java/com/faisal/dateTimeExtend/DateTime/ConvertionTest.java
@@ -6,14 +6,24 @@ import java.sql.Time;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+
+import javax.swing.text.DateFormatter;
+
 import java.util.*;
 
 import org.junit.Test;
-
+import java.util.TimeZone;
 public class ConvertionTest {
 
 	@Test
@@ -29,7 +39,6 @@ public class ConvertionTest {
 	public void calendarTimeToLocalTime()
 	{
 		Calendar cal = Calendar.getInstance();
-		
 		cal.set(Calendar.HOUR, 3);
 		cal.set(Calendar.MINUTE, 41);
 		cal.set(Calendar.SECOND, 50);
@@ -37,6 +46,8 @@ public class ConvertionTest {
 		LocalTime a = Convertion.convertCalanderTimeToLocalTime(cal);
 		assertEquals(LocalTime.of(3, 41, 50), a);
 	}
+	
+	
 	
 	@Test
 	public void dateToLocalDate() throws ParseException
@@ -68,6 +79,7 @@ public class ConvertionTest {
 	    Date date =  sdf.parse(dateValueString);
 	    
 	    assertEquals(LocalTime.of(5,35,15), Convertion.convertTimeToLocalTime(date));
+	    
 	    //System.out.println(Convertion.convertTimeToLocalTime(date));
 	}
 	
@@ -75,6 +87,8 @@ public class ConvertionTest {
 	public void LocalTimeToTime() throws ParseException
 	{
 		//incomplete
+		
+		/*
 		
 		LocalTime lt = LocalTime.of(5, 35, 15);
 		
@@ -85,8 +99,90 @@ public class ConvertionTest {
 	    //assertEquals(time, Convertion.convertLocalTimeToTime(lt));
 	    
 	    System.out.println(time);
+	    
+	    */
  	}
 	
-
+	
+	@Test
+	public void DateToOffsetDate()
+	{
+		Calendar cal = Calendar.getInstance();
+		ZoneOffset offset = ZoneOffset.ofHoursMinutes(1, 30);
+		OffsetDateTime dateTime1=OffsetDateTime.of(LocalDateTime.of(2019, 01, 12, 05, 45),
+				ZoneOffset.ofHoursMinutes(6, 30));
+		//assertNotEquals(dateTime1,Convertion.DateToOffsetDateTime(cal, offset));
+		//System.out.println(dateTime1);
+	}
+	
+	@Test
+	public void offsetDateToInstant()
+	{
+		OffsetDateTime offsetDateTime = OffsetDateTime.of(2014, 12, 03, 10, 15, 30, 0,
+				ZoneOffset.ofHours(+6));
+		Instant in1 = Instant.parse("2014-12-03T04:15:30.00Z");
+		//System.out.println("offsetDateTime = " + Instant.parse("2014-12-03T10:15:30.00Z"));
+		assertEquals(in1, Convertion.convertOffsetDateTimeToInstant(offsetDateTime));
+		
+	}
+	
+	@Test
+	public void offsetDateTimeToLocalDate()
+	{
+		OffsetDateTime offsetDateTime = OffsetDateTime.of(2014, 12, 03, 10, 15, 30, 0,
+				ZoneOffset.ofHours(+6));
+		
+		assertEquals(LocalDate.of(2014, 12, 03), Convertion.convertOffsetDateTimeToLocalDate(offsetDateTime));
+	}
+	
+	
+	
+	@Test
+	public void offsetDateTimeToLocalDateTime()
+	{
+		OffsetDateTime offsetDateTime = OffsetDateTime.of(2014, 12, 03, 10, 15, 30, 0,
+				ZoneOffset.ofHours(+6));
+		
+		assertEquals(LocalDateTime.of(2014, 12, 03,10,15,30), Convertion.convertOffsetDateTimeToLocalDateTime(offsetDateTime));
+	}
+	
+	@Test
+	public void localDateTimeToOffsetDateTime() {
+		
+		LocalDateTime date=LocalDateTime.of(2014, 12,3,10,15,30);
+		assertEquals(OffsetDateTime.of(2014, 12, 03, 10, 15, 30, 0,
+				ZoneOffset.ofHours(+6)), Convertion.convertLocalDateTimeToOffsetDateTime(date));
+		
+	}
+	
+	@Test
+	public void offsetDateTimeToLocalTime()
+	{
+		OffsetDateTime offsetDateTime = OffsetDateTime.of(2014, 12, 03, 10, 15, 30, 0,
+				ZoneOffset.ofHours(+6));
+		
+		assertEquals(LocalTime.of(10, 15, 30), Convertion.convertOffsetDateTimeToLocalTime(offsetDateTime));
+	}
+	
+	@Test
+	public void offsetDateTimeToOffsetTime()
+	{
+		OffsetDateTime offsetDateTime = OffsetDateTime.of(2014, 12, 03, 10, 15, 30, 0,
+				ZoneOffset.ofHours(+6));
+		
+		assertEquals(OffsetTime.of(10, 15,30,0, ZoneOffset.ofHours(6)), Convertion.convertOffsetDateTimeToOffsetTime(offsetDateTime));
+	}
+	
+	
+	@Test
+	public void caldateTimeTolocalDateTimetest() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(2022, Calendar.FEBRUARY + 1, 16);
+		cal.set(Calendar.HOUR, 3);
+		cal.set(Calendar.MINUTE, 30);
+		cal.set(Calendar.SECOND, 50);
+		assertEquals(LocalDateTime.of(2022, Month.FEBRUARY, 16,3,30,50), Convertion.convertCalanderToLocalDateTime(cal));
+	}
+	
 
 }
